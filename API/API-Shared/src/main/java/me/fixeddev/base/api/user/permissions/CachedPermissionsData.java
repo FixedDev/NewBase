93f908634f142312167f0c83debb17ab67d29121
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-09-07 22:53:55 -0500

    Don't save the retriever, it isn't used

--- API/API-Shared/src/main/java/me/fixeddev/base/api/user/permissions/CachedPermissionsData.java
+++ API/API-Shared/src/main/java/me/fixeddev/base/api/user/permissions/CachedPermissionsData.java
@@ -6,12 +6,9 @@ import java.util.concurrent.TimeUnit;
 
 public class CachedPermissionsData extends DelegatePermissionsData {
 
-    private PermissionDataRetriever retriever;
-
     public CachedPermissionsData(PermissionsData delegate, PermissionDataRetriever retriever) {
         super(delegate);
 
-        this.retriever = retriever;
         this.delegate = Suppliers.memoizeWithExpiration(() -> retriever.ofPlayerId(id()), 2, TimeUnit.MINUTES);
     }
 
