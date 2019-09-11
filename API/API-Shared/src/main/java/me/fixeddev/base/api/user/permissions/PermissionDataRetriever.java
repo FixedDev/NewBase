374222d51a1a12848f36539f496b0ada221ab0df
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-09-10 22:05:31 -0500

    Provide a method to save the changes in the permission data of the user

--- API/API-Shared/src/main/java/me/fixeddev/base/api/user/permissions/PermissionDataRetriever.java
+++ API/API-Shared/src/main/java/me/fixeddev/base/api/user/permissions/PermissionDataRetriever.java
@@ -2,4 +2,6 @@ package me.fixeddev.base.api.user.permissions;
 
 public interface PermissionDataRetriever {
     PermissionsData ofPlayerId(String id);
+
+    void save(PermissionsData permissionsData);
 }
