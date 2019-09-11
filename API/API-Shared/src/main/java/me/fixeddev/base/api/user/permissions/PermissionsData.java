18e5e549d7d136d9c326d46e07188442535411f5
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-09-10 22:05:09 -0500

    We shouldn't save the actual group in there

--- API/API-Shared/src/main/java/me/fixeddev/base/api/user/permissions/PermissionsData.java
+++ API/API-Shared/src/main/java/me/fixeddev/base/api/user/permissions/PermissionsData.java
@@ -2,13 +2,9 @@ package me.fixeddev.base.api.user.permissions;
 
 import me.fixeddev.base.api.datamanager.SavableObject;
 import me.fixeddev.base.api.permissions.Permissible;
-import me.fixeddev.base.api.permissions.group.Group;
-
-import java.util.List;
 
 public interface PermissionsData extends Permissible, SavableObject {
+    String getPrimaryGroup();
 
-    void setParents(List<Group> parents);
-
-    List<Group> getParents();
+    void setPrimaryGroup(String group);
 }
