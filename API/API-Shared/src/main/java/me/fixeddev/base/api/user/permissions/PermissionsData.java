f4040086f0dba4b1ec2222d72b9950eb158a2359
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-09-14 22:22:09 -0500

    Revert that change, we can't save the subject without causing a lot of problems
    Signed-off-by: Gilberto Garcia <gilbertodamian14@gmail.com>

--- API/API-Shared/src/main/java/me/fixeddev/base/api/user/permissions/PermissionsData.java
+++ API/API-Shared/src/main/java/me/fixeddev/base/api/user/permissions/PermissionsData.java
@@ -2,26 +2,12 @@ package me.fixeddev.base.api.user.permissions;
 
 import me.fixeddev.base.api.datamanager.SavableObject;
 import me.fixeddev.base.api.permissions.Permissible;
-import me.fixeddev.base.api.permissions.Tristate;
-import me.fixeddev.base.api.permissions.permission.Permission;
-import org.jetbrains.annotations.NotNull;
 
 import java.time.LocalTime;
-import java.util.List;
 
 public interface PermissionsData extends Permissible, SavableObject {
-
-    Object getSubject();
 
     String getPrimaryGroup();
 
     LocalTime getCalculationTime();
-
-    default Tristate hasPermission(@NotNull String permission) {
-        return hasPermission(permission, getSubject());
-    }
-
-    default List<Permission> getEffectivePermissions() {
-        return getEffectivePermissions(getSubject());
-    }
 }
