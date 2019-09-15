96d3b6eccc440f448e47f0be1c1a9871a213d3f1
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-09-14 21:19:26 -0500

    Add a better api for the user permissions

--- API/API-Shared/src/main/java/me/fixeddev/base/api/user/User.java
+++ API/API-Shared/src/main/java/me/fixeddev/base/api/user/User.java
@@ -1,7 +1,10 @@
 package me.fixeddev.base.api.user;
 
+import com.google.common.util.concurrent.ListenableFuture;
 import me.fixeddev.base.api.datamanager.SavableObject;
+import me.fixeddev.base.api.user.permissions.PermissionDataCalculator;
 import me.fixeddev.base.api.user.permissions.PermissionsData;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 import java.util.Optional;
@@ -24,5 +27,34 @@ public interface User extends SavableObject {
     boolean isInStaffChat();
     void setInStaffChat(boolean inStaffChat);
 
-    PermissionsData getPermissionsData();
+    /**
+     * @return The primary group of this user
+     * @throws IllegalStateException if the user doesn't has a primary group set already
+     */
+    String getPrimaryGroup();
+    void setPrimaryGroup(@NotNull String group);
+
+    /**
+     * Checks if this user has a cached instance of the {@link PermissionsData} and returns it
+     * @return an optional {@link PermissionsData} for this user,
+     *         absent if this user never calculated the {@link PermissionsData} or if the PermissionsData
+     *         was invalidated and never re calculated
+     */
+    Optional<PermissionsData> getPermissionData();
+
+    /**
+     * Retrieves the {@link PermissionsData} of this user with the specified {@link PermissionDataCalculator}
+     * And caches it on this user instance, so the {@link User#getPermissionData()} method can be used
+     *
+     * @param dataRetriever The {@link PermissionDataCalculator} to use to retrieve the {@link PermissionsData}
+     * @return A future PermissionsData that's never null, even if the data for this user doesn't exist
+     */
+    ListenableFuture<PermissionsData> calculatePermissionsData(@NotNull PermissionDataCalculator dataCalculator);
+
+    /**
+     * Invalidates the cached {@link PermissionsData} of this user
+     * After the invalidation of the cache the {@link User#getPermissionData()} method can't be used
+     * until the re-calculation of the {@link PermissionsData} with the method {@link #calculatePermissionsData(PermissionDataCalculator)}
+     */
+    void invalidatePermissionsData();
 }
