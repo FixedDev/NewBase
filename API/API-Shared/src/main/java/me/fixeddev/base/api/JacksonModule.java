2a3e9d21d52f1964eeb0e9fc440d937435157803
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-09-15 11:41:55 -0500

    Add a module for the jackson ObjectMappers
    Signed-off-by: Gilberto Garcia <gilbertodamian14@gmail.com>

---
+++ API/API-Shared/src/main/java/me/fixeddev/base/api/JacksonModule.java
@@ -0,0 +1,12 @@
+package me.fixeddev.base.api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.inject.Provides;
+import me.fixeddev.inject.ProtectedModule;
+
+public class JacksonModule extends ProtectedModule {
+    @Provides
+    public ObjectMapper provideObjectMapper(){
+        return new ObjectMapper();
+    }
+}
