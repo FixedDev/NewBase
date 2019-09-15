02a6aeaf2f49e7fbe0569a559d4c1356642125f4
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-09-14 22:44:35 -0500

    Allow the usage of a different executor
    Signed-off-by: Gilberto Garcia <gilbertodamian14@gmail.com>

--- API/API-Shared/src/main/java/me/fixeddev/base/api/future/FutureUtils.java
+++ API/API-Shared/src/main/java/me/fixeddev/base/api/future/FutureUtils.java
@@ -3,14 +3,22 @@ package me.fixeddev.base.api.future;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 
+import java.util.concurrent.Executor;
 import java.util.function.Function;
 
 public class FutureUtils {
+    private FutureUtils() {
+    }
+
     public static <V> void addCallback(ListenableFuture<V> future, Callback<V> callback) {
         Futures.addCallback(future, Callback.convertToGuavaCallback(callback));
     }
 
+    public static <V, R> ListenableFuture<R> transform(ListenableFuture<V> future, Function<V, R> transformFunction, Executor executor) {
+        return Futures.transform(future, transformFunction::apply, executor);
+    }
+
     public static <V, R> ListenableFuture<R> transform(ListenableFuture<V> future, Function<V, R> transformFunction) {
-        return Futures.transform(future, transformFunction::apply);
+        return Futures.transform(future, transformFunction::apply, Runnable::run);
     }
 }
