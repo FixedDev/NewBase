5daed58ea1e4ca694742d053914c4732e2fe7c3f
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-09-14 22:38:00 -0500

    Add transform function to the util because the ambiguity caused by guava
    Signed-off-by: Gilberto Garcia <gilbertodamian14@gmail.com>

--- API/API-Shared/src/main/java/me/fixeddev/base/api/future/FutureUtils.java
+++ API/API-Shared/src/main/java/me/fixeddev/base/api/future/FutureUtils.java
@@ -3,8 +3,14 @@ package me.fixeddev.base.api.future;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 
+import java.util.function.Function;
+
 public class FutureUtils {
     public static <V> void addCallback(ListenableFuture<V> future, Callback<V> callback) {
         Futures.addCallback(future, Callback.convertToGuavaCallback(callback));
+    }
+
+    public static <V, R> ListenableFuture<R> transform(ListenableFuture<V> future, Function<V, R> transformFunction) {
+        return Futures.transform(future, transformFunction::apply);
     }
 }
