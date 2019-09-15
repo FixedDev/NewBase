3b2862ed7c61b297b09e27f9e56c402c077c3f81
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-09-15 12:00:21 -0500

    Should be deserialized as PojoPermissionsData Also ignore the getPrimaryGroup and getCalculationTime fields because they're not needed
    Signed-off-by: Gilberto Garcia <gilbertodamian14@gmail.com>

--- API/API-Shared/src/main/java/me/fixeddev/base/api/user/permissions/PermissionsData.java
+++ API/API-Shared/src/main/java/me/fixeddev/base/api/user/permissions/PermissionsData.java
@@ -1,13 +1,18 @@
 package me.fixeddev.base.api.user.permissions;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import me.fixeddev.base.api.datamanager.SavableObject;
 import me.fixeddev.base.api.permissions.Permissible;
 
 import java.time.LocalTime;
 
+@JsonDeserialize(as = PojoPermissionsData.class)
 public interface PermissionsData extends Permissible, SavableObject {
 
+    @JsonIgnore
     String getPrimaryGroup();
 
+    @JsonIgnore
     LocalTime getCalculationTime();
 }
