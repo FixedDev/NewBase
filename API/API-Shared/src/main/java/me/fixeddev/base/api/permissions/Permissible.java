6689f0d3a45869c99acac71a2f40fc3161f3abe7
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-09-15 11:34:43 -0500

    Add a bit of documentation for the getPermissions and getEffectivePermissions methods
    Signed-off-by: Gilberto Garcia <gilbertodamian14@gmail.com>

--- API/API-Shared/src/main/java/me/fixeddev/base/api/permissions/Permissible.java
+++ API/API-Shared/src/main/java/me/fixeddev/base/api/permissions/Permissible.java
@@ -11,9 +11,19 @@ public interface Permissible {
 
     void setPermission(Permission permission);
 
+    /**
+     * @return The specific permissions for this permissible, not counting the parent permissions
+     */
     @NotNull
     List<Permission> getPermissions();
 
+    /**
+     * Searches through the permissions of this permissible and his parents
+     * and calculates the contexts for every {@link Permission} with the specified subject
+     * 
+     * @param subject The subject to use when calculating the contexts
+     * @return A list of the effective permissions on this context
+     */
     @NotNull
     List<Permission> getEffectivePermissions(Object subject);
 }
