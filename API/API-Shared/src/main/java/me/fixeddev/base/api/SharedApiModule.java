6a4629f08292caa4c95824aa3f75bda0d3eacceb
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-09-15 11:42:27 -0500

    Install the JacksonModule in the SharedApiModule
    Signed-off-by: Gilberto Garcia <gilbertodamian14@gmail.com>

--- API/API-Shared/src/main/java/me/fixeddev/base/api/SharedApiModule.java
+++ API/API-Shared/src/main/java/me/fixeddev/base/api/SharedApiModule.java
@@ -5,6 +5,7 @@ import me.fixeddev.inject.ProtectedModule;
 public class SharedApiModule extends ProtectedModule {
     @Override
     protected void configure() {
+        install(new JacksonModule());
         install(new DatabaseModule());
         install(new ManagersModule());
     }
