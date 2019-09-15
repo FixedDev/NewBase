8a3c950e705f10047cb297ca96c801f934154f9b
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-09-15 12:08:10 -0500

    Install the PermissionsDataModule
    Signed-off-by: Gilberto Garcia <gilbertodamian14@gmail.com>

--- API/API-Shared/src/main/java/me/fixeddev/base/api/user/UserModule.java
+++ API/API-Shared/src/main/java/me/fixeddev/base/api/user/UserModule.java
@@ -1,13 +1,15 @@
 package me.fixeddev.base.api.user;
 
 import me.fixeddev.base.api.inject.DataManagerBinder;
+import me.fixeddev.base.api.user.permissions.PermissionsDataModule;
 import me.fixeddev.inject.ProtectedModule;
 
 public class UserModule extends ProtectedModule {
     @Override
     protected void configure() {
+        install(new PermissionsDataModule());
+
         DataManagerBinder binder = DataManagerBinder.createBinder(binder());
         binder.bindDataManager(User.class, "user");
-
     }
 }
