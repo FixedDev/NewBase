c4b20b6b3ac3e4451b3535bf08df2ac9518a6297
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-09-15 11:44:54 -0500

    Create an abstract version for the PermissionDataCalculator Why abstract? Well because we need to get the player of the plataform that's the base running on and we can't get it through a really generic code, the getSubjectForUser method should return that
    Signed-off-by: Gilberto Garcia <gilbertodamian14@gmail.com>

---
+++ API/API-Shared/src/main/java/me/fixeddev/base/api/user/permissions/AbstractPermissionDataCalculator.java
@@ -0,0 +1,54 @@
+package me.fixeddev.base.api.user.permissions;
+
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.ListeningExecutorService;
+import me.fixeddev.base.api.datamanager.ObjectRepository;
+import me.fixeddev.base.api.datamanager.RedisCache;
+import me.fixeddev.base.api.future.FutureUtils;
+import me.fixeddev.base.api.permissions.group.Group;
+import me.fixeddev.base.api.permissions.group.GroupManager;
+import me.fixeddev.base.api.user.User;
+
+public abstract class AbstractPermissionDataCalculator implements PermissionDataCalculator {
+
+    private RedisCache<PermissionsData> redisCache;
+    private ObjectRepository<PermissionsData> objectRepository;
+
+    private GroupManager groupManager;
+
+    private ListeningExecutorService executorService;
+
+    protected AbstractPermissionDataCalculator(RedisCache<PermissionsData> redisCache, ObjectRepository<PermissionsData> objectRepository, GroupManager groupManager, ListeningExecutorService executorService) {
+        this.redisCache = redisCache;
+        this.objectRepository = objectRepository;
+        this.groupManager = groupManager;
+        this.executorService = executorService;
+    }
+
+    @Override
+    public ListenableFuture<PermissionsData> calculateForUser(User user) {
+        return FutureUtils.transformAsync(redisCache.getOrFind(user.id()), permissionsData -> {
+            String primaryGroupName = user.getPrimaryGroup();
+            Group group = groupManager.getGroupByName(primaryGroupName).get();
+            Object subject = getSubjectForUser(user);
+
+            SimplePermissionsData newPermissionsData = new SimplePermissionsData(user.id(), group, subject);
+
+            if (permissionsData.isPresent()) {
+                PermissionsData data = permissionsData.get();
+
+                newPermissionsData.setPermissions(data.getPermissions());
+            }
+
+            return Futures.immediateFuture(newPermissionsData);
+        }, executorService);
+    }
+
+    @Override
+    public void save(PermissionsData permissionsData) {
+        objectRepository.save(permissionsData);
+    }
+
+    protected abstract Object getSubjectForUser(User user);
+}
