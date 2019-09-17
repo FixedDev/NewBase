46571c2b9f521d25f0f5aaef7ec058b7d5d5c327
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-09-16 22:03:20 -0500

    Create a binding for ExecutorService and ListeningExecutorService
    Signed-off-by: Gilberto Garcia <gilbertodamian14@gmail.com>

--- API/API-Shared/src/main/java/me/fixeddev/base/api/SharedApiModule.java
+++ API/API-Shared/src/main/java/me/fixeddev/base/api/SharedApiModule.java
@@ -1,12 +1,28 @@
 package me.fixeddev.base.api;
 
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.MoreExecutors;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
 import me.fixeddev.inject.ProtectedModule;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class SharedApiModule extends ProtectedModule {
     @Override
     protected void configure() {
+        bind(ExecutorService.class).to(ListeningExecutorService.class);
+
         install(new JacksonModule());
         install(new DatabaseModule());
         install(new ManagersModule());
     }
+
+    @Provides
+    @Singleton
+    public ListeningExecutorService provideExecutorService() {
+        return MoreExecutors.listeningDecorator(Executors.newFixedThreadPool(10));
+    }
 }
+
