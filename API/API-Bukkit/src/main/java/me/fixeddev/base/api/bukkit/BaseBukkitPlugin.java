d748ff2410306b974bfd961f054a14c54eddfe47
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-09-18 23:31:35 -0500

    Start all the services on the onEnable
    Signed-off-by: Gilberto Garcia <gilbertodamian14@gmail.com>

--- API/API-Bukkit/src/main/java/me/fixeddev/base/api/bukkit/BaseBukkitPlugin.java
+++ API/API-Bukkit/src/main/java/me/fixeddev/base/api/bukkit/BaseBukkitPlugin.java
@@ -1,11 +1,31 @@
 package me.fixeddev.base.api.bukkit;
 
+import com.google.inject.Inject;
+import me.fixeddev.base.api.service.ServiceManager;
 import me.fixeddev.inject.ProtectedBinder;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Level;
+
 public class BaseBukkitPlugin extends JavaPlugin {
+
+    @Inject
+    private ServiceManager serviceManager;
+
+
     @Override
     public void configure(ProtectedBinder binder) {
         binder.install(new ApiBukkitModule(getConfig()));
+    }
+
+    @Override
+    public void onEnable() {
+        try {
+            serviceManager.start();
+        } catch (Exception e) {
+            getLogger().log(Level.SEVERE,"Failed to start services, stopping server", e);
+            Bukkit.getServer().shutdown();
+        }
     }
 }
