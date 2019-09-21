17db1f9b6ce438f8770d7313cd4e4d93801d19b1
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-09-20 22:14:40 -0500

    Unimplemented the Weightable interface on the group class, since a group having weight makes no sense if a user only can have one group
    Signed-off-by: Gilberto Garcia <gilbertodamian14@gmail.com>

--- API/API-Shared/src/main/java/me/fixeddev/base/api/permissions/group/Group.java
+++ API/API-Shared/src/main/java/me/fixeddev/base/api/permissions/group/Group.java
@@ -5,10 +5,8 @@ import com.google.common.collect.ImmutableSet;
 import me.fixeddev.base.api.datamanager.SavableObject;
 import me.fixeddev.base.api.datamanager.meta.ObjectName;
 import me.fixeddev.base.api.permissions.AbstractPermissible;
-import me.fixeddev.base.api.permissions.Contextable;
 import me.fixeddev.base.api.permissions.Permissible;
 import me.fixeddev.base.api.permissions.Weightable;
-import me.fixeddev.base.api.permissions.context.Context;
 import me.fixeddev.base.api.permissions.Permission;
 
 import java.util.ArrayList;
@@ -16,13 +14,11 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Optional;
-import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Stream;
 
 @ObjectName("group")
-public class Group extends AbstractPermissible implements Weightable, Contextable, Permissible, SavableObject {
+public class Group extends AbstractPermissible implements Permissible, SavableObject {
 
     private final String groupName;
     private String prefix;
@@ -32,32 +28,24 @@ public class Group extends AbstractPermissible implements Weightable, Contextabl
 
     private Map<String,List<Permission>> permissionList;
 
-    private Map<String, Context> contexts;
-
-    private int weight;
-
-    Group(String groupName, String prefix, String suffix, Map<String, List<Permission>> permissionList, List<Group> parents, Map<String, Context> contexts, int weight) {
+    Group(String groupName, String prefix, String suffix, Map<String, List<Permission>> permissionList, List<Group> parents) {
         this.groupName = groupName;
         this.prefix = prefix;
         this.suffix = suffix;
         this.parents = parents;
         this.permissionList = permissionList;
-        this.contexts = contexts;
-        this.weight = weight;
     }
 
-    Group(String groupName, String prefix, String suffix, int weight) {
+    Group(String groupName, String prefix, String suffix) {
         this.groupName = groupName;
         this.prefix = prefix;
         this.suffix = suffix;
         this.parents = new ArrayList<>();
         this.permissionList = new ConcurrentHashMap<>();
-        this.contexts = new ConcurrentHashMap<>();
-        this.weight = weight;
     }
 
     Group(String name, int weight) {
-        this(name, "", "", weight);
+        this(name, "", "");
     }
 
     Group(String groupName) {
@@ -94,16 +82,6 @@ public class Group extends AbstractPermissible implements Weightable, Contextabl
     }
 
     @Override
-    public int getWeight() {
-        return weight;
-    }
-
-    @Override
-    public void setWeight(int weight) {
-        this.weight = weight;
-    }
-
-    @Override
     protected Map<String, List<Permission>> getRawPermissionList() {
         return permissionList;
     }
@@ -117,43 +95,19 @@ public class Group extends AbstractPermissible implements Weightable, Contextabl
     }
 
     @Override
-    public Set<String> getAllContextsKeys() {
-        return new ImmutableSet.Builder<String>().addAll(contexts.keySet()).build();
-    }
-
-    @Override
-    public Set<Context> getAllContexts() {
-        return new ImmutableSet.Builder<Context>().addAll(contexts.values()).build();
-    }
-
-    @Override
-    public Optional<Context> getContext(String key) {
-        return Optional.ofNullable(contexts.get(key));
-    }
-
-    @Override
-    public void addContext(Context context) {
-        Preconditions.checkNotNull(context, "You can't add null contexts");
-
-        contexts.put(context.getKey(), context);
-    }
-
-    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Group)) return false;
         Group group = (Group) o;
-        return weight == group.weight &&
-                groupName.equals(group.groupName) &&
+        return groupName.equals(group.groupName) &&
                 Objects.equals(prefix, group.prefix) &&
                 Objects.equals(suffix, group.suffix) &&
                 permissionList.equals(group.permissionList) &&
-                parents.equals(group.parents) &&
-                contexts.equals(group.contexts);
+                parents.equals(group.parents);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(groupName, prefix, suffix, permissionList, parents, contexts, weight);
+        return Objects.hash(groupName, prefix, suffix, permissionList, parents);
     }
 }
