32e4144c304f288f3e5d0b91da7f8da5e24c38f9
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-09-25 18:56:23 -0500

    Make the data manager requests serializable
    Signed-off-by: Gilberto Garcia <gilbertodamian14@gmail.com>

--- API/API-Shared/src/main/java/me/fixeddev/base/api/datamanager/DeleteCachedObjectRequest.java
+++ API/API-Shared/src/main/java/me/fixeddev/base/api/datamanager/DeleteCachedObjectRequest.java
@@ -1,19 +1,13 @@
 package me.fixeddev.base.api.datamanager;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
+@AllArgsConstructor @Getter
 public class DeleteCachedObjectRequest<O extends SavableObject> {
     @NotNull
     private List<O> deletedObjects;
-
-    public DeleteCachedObjectRequest(@NotNull List<O> deletedObjects) {
-        this.deletedObjects = deletedObjects;
-    }
-
-    @NotNull
-    public List<O> getDeletedObjects() {
-        return deletedObjects;
-    }
 }
--- API/API-Shared/src/main/java/me/fixeddev/base/api/datamanager/UpdateCachedObjectRequest.java
+++ API/API-Shared/src/main/java/me/fixeddev/base/api/datamanager/UpdateCachedObjectRequest.java
@@ -1,19 +1,13 @@
 package me.fixeddev.base.api.datamanager;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Map;
 
+@AllArgsConstructor @Getter
 public class UpdateCachedObjectRequest<O extends SavableObject> {
     @NotNull
     private Map<String, O> updatedObjects;
-
-    public UpdateCachedObjectRequest(@NotNull Map<String, O> updatedObjects) {
-        this.updatedObjects = updatedObjects;
-    }
-
-    @NotNull
-    public Map<String, O> getUpdatedObjects() {
-        return updatedObjects;
-    }
 }
