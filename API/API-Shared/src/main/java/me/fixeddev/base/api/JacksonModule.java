39884bebd7446655d8a2008ac09d6ae54851f0e9
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-09-28 22:33:03 -0500

    Register the Jdk8Module and JavaTimeModule
    Signed-off-by: Gilberto Garcia <gilbertodamian14@gmail.com>

--- API/API-Shared/src/main/java/me/fixeddev/base/api/JacksonModule.java
+++ API/API-Shared/src/main/java/me/fixeddev/base/api/JacksonModule.java
@@ -1,13 +1,15 @@
 package me.fixeddev.base.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.google.inject.Provides;
 import me.fixeddev.inject.ProtectedModule;
 
 public class JacksonModule extends ProtectedModule {
     @Provides
     public ObjectMapper provideObjectMapper(){
-        return new ObjectMapper();
+        return new ObjectMapper().registerModules(new Jdk8Module(), new JavaTimeModule());
     }
 
     @Override
