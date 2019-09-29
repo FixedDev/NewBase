8a7335504c21bcd0101b4de641371eb9149c3d81
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-09-28 19:51:45 -0500

    Make the ObjectWrapper serializable with jackson
    Signed-off-by: Gilberto Garcia <gilbertodamian14@gmail.com>

--- API/API-Shared/src/main/java/me/fixeddev/base/api/messager/ObjectWrapper.java
+++ API/API-Shared/src/main/java/me/fixeddev/base/api/messager/ObjectWrapper.java
@@ -1,24 +1,12 @@
 package me.fixeddev.base.api.messager;
 
-import java.beans.ConstructorProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 import java.util.UUID;
 
+@AllArgsConstructor @Data
 public class ObjectWrapper<O> {
     private final O object;
-
     private final UUID channelInstanceId;
-
-    @ConstructorProperties({"object","channelInstanceId"})
-    ObjectWrapper(O object, UUID channelInstanceId) {
-        this.object = object;
-        this.channelInstanceId = channelInstanceId;
-    }
-
-    public O getObject() {
-        return object;
-    }
-
-    public UUID getChannelInstanceId() {
-        return channelInstanceId;
-    }
 }
