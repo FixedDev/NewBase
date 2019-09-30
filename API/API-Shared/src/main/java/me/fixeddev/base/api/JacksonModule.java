735eb274cda8d02bf3d9721cae0d77c7b53fa431
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-09-29 20:38:30 -0500

    The ObjectMapper should be a singleton
    Signed-off-by: Gilberto Garcia <gilbertodamian14@gmail.com>

--- API/API-Shared/src/main/java/me/fixeddev/base/api/JacksonModule.java
+++ API/API-Shared/src/main/java/me/fixeddev/base/api/JacksonModule.java
@@ -4,10 +4,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.google.inject.Provides;
+import com.google.inject.Singleton;
 import me.fixeddev.inject.ProtectedModule;
 
 public class JacksonModule extends ProtectedModule {
     @Provides
+    @Singleton
     public ObjectMapper provideObjectMapper(){
         return new ObjectMapper().registerModules(new Jdk8Module(), new JavaTimeModule());
     }
