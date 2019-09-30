c9c68bcb9cb55a52a386476eb2f6065294be4e7f
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-09-29 22:54:46 -0500

    Bind the BaseTranslationManager
    Signed-off-by: Gilberto Garcia <gilbertodamian14@gmail.com>

--- Commons/Commons-Shared/src/main/java/me/fixeddev/base/commons/translations/TranslationsModule.java
+++ Commons/Commons-Shared/src/main/java/me/fixeddev/base/commons/translations/TranslationsModule.java
@@ -1,10 +1,11 @@
 package me.fixeddev.base.commons.translations;
 
+import com.google.inject.Scopes;
 import me.fixeddev.inject.ProtectedModule;
 
 public class TranslationsModule extends ProtectedModule {
     @Override
     protected void configure() {
-        bind(TranslationManager.class);
+        bind(TranslationManager.class).to(BaseTranslationManager.class).in(Scopes.SINGLETON);
     }
 }
