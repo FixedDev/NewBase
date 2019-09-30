4e82aeb7f099e50a6f16508eda8e1339e1da9c3e
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-09-29 21:03:33 -0500

    Add support to wildcards
    Signed-off-by: Gilberto Garcia <gilbertodamian14@gmail.com>

--- API/API-Shared/src/main/java/me/fixeddev/base/api/permissions/AbstractPermissible.java
+++ API/API-Shared/src/main/java/me/fixeddev/base/api/permissions/AbstractPermissible.java
@@ -9,16 +9,24 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public abstract class AbstractPermissible implements Permissible {
 
+    // This map is used to accelerate the wildcard matches
+    // This will match a permission to a wildcard, so we don't need to use regex more than 1 time
+    // example: "test.hey" -> "test.*"
+    private static Map<String, String> permissionToWildcard = new ConcurrentHashMap<>();
+
     @NotNull
     @Override
     public Tristate hasPermission(@NotNull String permission, Object subject) {
-        Optional<Permission> optionalPermission = getEffectivePermissions(subject).stream()
-                .filter(perm -> perm.getName().equals(permission))
+        List<Permission> effectivePermissions = getEffectivePermissions(subject);
+
+        Optional<Permission> optionalPermission = effectivePermissions.stream()
+                .filter(perm -> match(permission, perm.getName()))
                 .max(Comparator.comparingInt(Permission::getWeight));
 
         if (!optionalPermission.isPresent()) {
@@ -56,6 +64,22 @@ public abstract class AbstractPermissible implements Permissible {
     }
 
     protected abstract Map<String, List<Permission>> getRawPermissionList();
+
+
+    // This method is used to match the wildcards
+    private boolean match(String text, String pattern) {
+        if (pattern.equals(permissionToWildcard.get(text))) {
+            return true;
+        }
+
+        boolean matches = text.matches(pattern.replace("?", ".?").replace("*", ".*?"));
+
+        if (matches) {
+            permissionToWildcard.put(text, pattern);
+        }
+
+        return matches;
+    }
 
     /**
      * @return A stream for the permissions of the parents for this permissible
