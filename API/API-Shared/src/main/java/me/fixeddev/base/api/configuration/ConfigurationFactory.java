b7f1cbfefe4b24d3d183df8e805ab64cef8c93a7
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-09-29 22:27:37 -0500

    Allow the throwing of a IOException on the save operation, since the save operation can fail
    Signed-off-by: Gilberto Garcia <gilbertodamian14@gmail.com>

--- API/API-Shared/src/main/java/me/fixeddev/base/api/configuration/ConfigurationFactory.java
+++ API/API-Shared/src/main/java/me/fixeddev/base/api/configuration/ConfigurationFactory.java
@@ -3,9 +3,10 @@ package me.fixeddev.base.api.configuration;
 import me.fixeddev.minecraft.config.Configuration;
 
 import java.io.File;
+import java.io.IOException;
 
 public interface ConfigurationFactory {
     Configuration getConfig(File file);
 
-    void saveConfiguration(Configuration configuration, File file);
+    void saveConfiguration(Configuration configuration, File file) throws IOException;
 }
