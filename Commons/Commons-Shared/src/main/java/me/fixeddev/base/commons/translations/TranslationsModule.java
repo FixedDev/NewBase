72c2fdb095d3d7c81036f7824e898bf86a757fa0
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-10-01 21:39:31 -0500

    Added a comment explaining why the fuck the TranslationManager is exposed
    Signed-off-by: Gilberto Garcia <gilbertodamian14@gmail.com>

--- Commons/Commons-Shared/src/main/java/me/fixeddev/base/commons/translations/TranslationsModule.java
+++ Commons/Commons-Shared/src/main/java/me/fixeddev/base/commons/translations/TranslationsModule.java
@@ -8,6 +8,10 @@ public class TranslationsModule extends ProtectedModule {
     protected void configure() {
         bind(TranslationManager.class).to(BaseTranslationManager.class).in(Scopes.SINGLETON);
 
+        // Well, if the ConfigurationFactory is not exposed, then the TranslationManager
+        // Is gonna use the ConfigurationFactory of the Commons
+        // You may ask why this is in this way, well, that's because I think that all the
+        // translations should go into the Commons
         expose(TranslationManager.class);
     }
 }
