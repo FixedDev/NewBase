07f339e900deb3cb978b14b53509f2d2ebf8b22a
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-10-01 21:35:36 -0500

    Remove unused imports
    Signed-off-by: Gilberto Garcia <gilbertodamian14@gmail.com>

--- API/API-Bukkit/src/main/java/me/fixeddev/base/api/bukkit/configuration/BukkitConfigurationFactoryModule.java
+++ API/API-Bukkit/src/main/java/me/fixeddev/base/api/bukkit/configuration/BukkitConfigurationFactoryModule.java
@@ -1,9 +1,7 @@
 package me.fixeddev.base.api.bukkit.configuration;
 
-import com.google.inject.Scopes;
 import com.google.inject.multibindings.OptionalBinder;
 import me.fixeddev.base.api.configuration.ConfigurationFactory;
-import me.fixeddev.base.api.configuration.NullConfigurationFactory;
 import me.fixeddev.inject.ProtectedModule;
 
 public class BukkitConfigurationFactoryModule extends ProtectedModule {
