11d76d7f9e17076f5b7ad777651dad2a594037ec
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-10-01 21:36:58 -0500

    Install the ConfigurationFactoryModule on there
    Signed-off-by: Gilberto Garcia <gilbertodamian14@gmail.com>

--- Commons/Commons-Shared/src/main/java/me/fixeddev/base/commons/CommonsModule.java
+++ Commons/Commons-Shared/src/main/java/me/fixeddev/base/commons/CommonsModule.java
@@ -1,11 +1,13 @@
 package me.fixeddev.base.commons;
 
+import me.fixeddev.base.api.configuration.ConfigurationFactoryModule;
 import me.fixeddev.base.commons.translations.TranslationsModule;
 import me.fixeddev.inject.ProtectedModule;
 
 public class CommonsModule extends ProtectedModule {
     @Override
     protected void configure() {
+        install(new ConfigurationFactoryModule());
         install(new TranslationsModule());
     }
 }
