df1c506eb6f52d14c670e8ee327a681219636839
Gilberto Garcia <gilbertodamian14@gmail.com>
2019-10-03 19:06:12 -0500

    The nested messages should be replaced first
    Signed-off-by: Gilberto Garcia <gilbertodamian14@gmail.com>

--- Commons/Commons-Shared/src/main/java/me/fixeddev/base/commons/translations/BaseTranslatableMessage.java
+++ Commons/Commons-Shared/src/main/java/me/fixeddev/base/commons/translations/BaseTranslatableMessage.java
@@ -4,12 +4,16 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class BaseTranslatableMessage implements TranslatableMessage {
 
     private String path;
     private Map<String, String> variableValues;
     private TranslationProvider translationProvider;
+
+    public static final Pattern NESTED_MESSAGE_FIND_PATTERN = Pattern.compile("\\[(\\w+)\\]");
 
     BaseTranslatableMessage(String path, TranslationProvider translationProvider) {
         this.path = path;
@@ -28,10 +32,29 @@ public class BaseTranslatableMessage implements TranslatableMessage {
     @Override
     public String getMessageForLang(@NotNull String lang) {
         String translation = translationProvider.getTranslation(lang, path);
+
+        if(translation == null || translation.isEmpty()){
+            return "";
+        }
+
+        Matcher translationMatcher = NESTED_MESSAGE_FIND_PATTERN.matcher(translation);
+
+        for (int i = 0; translationMatcher.find(); i++) {
+            String nestedPath = translationMatcher.group(i);
+
+            String nestedMessage = translationProvider.getTranslation(lang, nestedPath);
+
+            if(nestedMessage == null || nestedMessage.isEmpty()){
+                continue;
+            }
+
+            translation = translation.replace("[" + nestedPath + "]", nestedMessage);
+        }
+
         for (String key : variableValues.keySet()) {
             String value = variableValues.get(key);
 
-            translation = translation.replace("{"+key+"}", value);
+            translation = translation.replace("{" + key + "}", value);
         }
 
         return translation;
